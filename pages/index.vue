71b369cc05121e1dc1a967e14ba6dc7a73c26f3d
shiguma127 <39233270+shiguma127@users.noreply.github.com>
2020-04-10 13:47:30 +0900

    baseLayouts

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,57 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-container>
+        <v-row dense>
+          <v-col v-for="(item, i) in items" :key="i" cols="12">
+            <v-card :color="item.color" dark>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <v-avatar class="ma-3" size="125">
+                  <v-img :src="item.icon"></v-img>
+                </v-avatar>
+                <div>
+                  <v-card-text
+                    class="headline"
+                    v-text="item.title"
+                  ></v-card-text>
+                  <v-card-subtitle v-text="item.song"></v-card-subtitle>
+                  <v-card-text v-text="item.artist"></v-card-text>
+                </div>
+                <v-avatar class="ma-3" size="125" tile>
+                  <v-img :src="item.albumArt"></v-img>
+                </v-avatar>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+  data: () => ({
+    items: [
+      {
+        color: '#1F7087',
+        icon:
+          'https://pbs.twimg.com/profile_images/1161252458871644161/ckBHovuv_400x400.jpg',
+        albumArt: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: '@shiguma127 is Playing',
+        artist: 'Foster the People',
+        song: 'hahahahaSong'
+      },
+      {
+        color: '#952175',
+        icon:
+          'https://pbs.twimg.com/profile_images/1161252458871644161/ckBHovuv_400x400.jpg',
+        albumArt: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: '@shiguma128 is Playing',
+        artist: 'Ellie Goulding',
+        song: 'lalalaSong'
+      }
+    ]
+  })
 }
 </script>
