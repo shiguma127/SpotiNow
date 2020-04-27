fbb75044746aab4bf1a790552fe121200822ad63
shiguma127 <39233270+shiguma127@users.noreply.github.com>
2020-04-27 21:09:10 +0900

    add Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: 'Spotinow',
-    // titleTemplate: '%s - ' + process.env.npm_package_name,
+    // titleTemplate: 'Spotinow',
+    titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
